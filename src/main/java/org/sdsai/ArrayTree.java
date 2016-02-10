900cfa4d73516503a2a6d218621040d01ec9fdea
Sam Baskinger <basking2@yahoo.com>
2016-02-10 10:32:39 -0600

    ArrayTree: Adding tool to navigate a tree backed by an array.

---
+++ src/main/java/org/sdsai/ArrayTree.java
@@ -0,0 +1,109 @@
+package org.sdsai;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * A tree backed by an array.
+ *
+ * Because this tree is backed by an array, the tree is full.
+ * That is to say, when you iterate over it, every node value will be
+ * handed back whether it is defined or null.
+ *
+ * The user of this should take care to filter out values
+ * they do not want or consider undefined.
+ */
+public class ArrayTree<T> implements Iterable<T> {
+
+    private final ArrayList<T> tree;
+    private final int branchiness;
+
+    public ArrayTree(final int capacity, final int branchiness) {
+        this.tree = new ArrayList<T>(capacity);
+        this.branchiness = branchiness;
+    }
+
+    public T get(final int index) {
+        return tree.get(index);
+    }
+
+    public void set(final int index, final T t) {
+        tree.set(index, t);
+    }
+
+    public int parent(final int index) {
+        return parent(index, branchiness);
+    }
+
+    public int child(final int index, final int childNum) {
+        return child(index, childNum, branchiness);
+    }
+
+    public int child0(final int index) {
+        return child0(index, branchiness);
+    }
+
+    /**
+     * Iterate over every node in breadth-first order.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return tree.iterator();
+    }
+
+    public static int parent(final int index, final int branchiness) {
+        final int level       = indexLevel(index, branchiness);
+        final int levelOffset = levelOffset(level, branchiness);
+
+        // Previous siblings to the parent node.
+        final int previousSiblings = (index - levelOffset) / branchiness;
+
+        return levelOffset(level-1, branchiness) + previousSiblings; 
+    }
+
+    public static int child0(final int index, final int branchiness) {
+        final int level       = indexLevel(index, branchiness);
+        final int levelOffset = levelOffset(level, branchiness);
+
+        // Previous siblings to the child node.
+        final int previousSiblings = branchiness * (index - levelOffset);
+
+        return (int) (levelOffset(level+1, branchiness)  + previousSiblings);
+    }
+
+    public static int child(final int index, final int childNum, final int branchiness) {
+        final int child0 = child0(index, branchiness);
+
+        return child0 + childNum;
+    }
+
+    public int levelOffset(final int level) {
+        return levelOffset(level, branchiness);
+    }
+
+    public int indexLevel(final int index) {
+        return indexLevel(index, branchiness);
+    }
+
+    /**
+     * Starting at level 0, this returns the number of nodes at in a level.
+     */
+    public static int nodesAtLevel(final int level, final int branchiness) {
+        return (int)Math.pow(branchiness, level);
+    }
+
+    /**
+     * What is the offset into an array that the first node of a tree level occures at.
+     */
+    public static int levelOffset(final int level, final int branchiness) {
+        final double d = (Math.pow(branchiness, level)-1) / (branchiness-1);
+
+        return (int) d;
+    }
+
+    public static int indexLevel(final int index, final int branchiness) {
+        final double n = Math.log(index * (branchiness - 1) + 1);
+        final double d = Math.log(branchiness);
+        return (int)(n / d);
+    }
+}
---
+++ src/test/java/org/sdsai/ArrayTreeTest.java
@@ -0,0 +1,44 @@
+package org.sdsai;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+public class ArrayTreeTest
+{
+  @Test(groups = {"sdsai"})
+  public void testIndexLevel() 
+  {
+    Assert.assertEquals(0, ArrayTree.indexLevel(0, 3));
+    Assert.assertEquals(1, ArrayTree.indexLevel(1, 3));
+    Assert.assertEquals(1, ArrayTree.indexLevel(3, 3));
+    Assert.assertEquals(2, ArrayTree.indexLevel(4, 3));
+    Assert.assertEquals(2, ArrayTree.indexLevel(5, 3));
+    Assert.assertEquals(2, ArrayTree.indexLevel(12, 3));
+    Assert.assertEquals(3, ArrayTree.indexLevel(13, 3));
+  }
+
+  @Test(groups={"sdsai"})
+  public void testLevelOffset() 
+  {
+    Assert.assertEquals(0, ArrayTree.levelOffset(0, 3));
+    Assert.assertEquals(1, ArrayTree.levelOffset(1, 3));
+    Assert.assertEquals(4, ArrayTree.levelOffset(2, 3));
+    Assert.assertEquals(13, ArrayTree.levelOffset(3, 3));
+  }
+
+  @Test(groups={"sdsai"})
+  public void testParent() 
+  {
+    Assert.assertEquals(0, ArrayTree.parent(0, 3));
+
+    Assert.assertEquals(0, ArrayTree.parent(1, 3));
+    Assert.assertEquals(0, ArrayTree.parent(2, 3));
+    Assert.assertEquals(0, ArrayTree.parent(3, 3));
+
+    Assert.assertEquals(1, ArrayTree.parent(4, 3));
+    Assert.assertEquals(2, ArrayTree.parent(7, 3));
+    Assert.assertEquals(3, ArrayTree.parent(10, 3));
+  }
+}
+
