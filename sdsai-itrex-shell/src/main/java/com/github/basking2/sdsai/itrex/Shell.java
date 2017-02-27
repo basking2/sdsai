2578d0cb2ee3f825d71f3f47d5d78aca66cd72fc
Sam <basking2@yahoo.com>
2017-02-27 09:04:45 -0600

    Shell: Isolate the Shell code into its own project for now.

--- sdsai-itrex-shell/src/main/java/com/github/basking2/sdsai/itrex/Shell.java
+++ sdsai-itrex-shell/src/main/java/com/github/basking2/sdsai/itrex/Shell.java
@@ -1,6 +1,8 @@
 package com.github.basking2.sdsai.itrex;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.Executors;
 
 /**
