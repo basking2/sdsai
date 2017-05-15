fe1efaf1ef53c9aec863094c05c820448f95358c
Sam <basking2@yahoo.com>
2017-05-15 16:07:27 -0500

    publish.sh script.

---
+++ scripts/publish.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Publish the website.
+
+set -x
+set -e
+
+git pull -r
+./gradlew clean check asciidoctor install
+git checkout gh-pages
+
+for proj in sdsai-common sdsai-itrex sdsai-itrex-shell sdsai-net sdsai-sandbox
+do
+	rm -fr "javadocs/$proj" || true
+	mv "$proj/build/docs/javadoc" "javadocs/$proj" || true
+
+	rm -fr"docs/$proj" || true
+	mv "$proj/build/asciidoc/html5" "docs/$proj" || true
+done
+
+echo Page updated. Make final edits.
