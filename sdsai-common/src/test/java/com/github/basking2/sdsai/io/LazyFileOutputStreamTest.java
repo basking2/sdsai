ff21939fd6f84b4d55b90937411c819009e4ec0d
Sam <basking2@yahoo.com>
2019-05-16 07:30:50 -0500

    Extending a test.

--- sdsai-common/src/test/java/com/github/basking2/sdsai/io/LazyFileOutputStreamTest.java
+++ sdsai-common/src/test/java/com/github/basking2/sdsai/io/LazyFileOutputStreamTest.java
@@ -2,9 +2,11 @@ package com.github.basking2.sdsai.io;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class LazyFileOutputStreamTest {
     @Test
@@ -42,5 +44,43 @@ public class LazyFileOutputStreamTest {
             f.delete();
         }
 
+    }
+
+    @Test
+    public void testReading() throws IOException {
+
+        final File dir = Files.createTempDirectory("filteringtest").toFile();
+
+        final FileRing fileRing = new FileRing(dir, "", "", 3);
+
+        final OutputStream os = new LazyOutputStream(1, new LazyOutputStream.OutputStreamFactory() {
+            @Override
+            public OutputStream get() throws IOException {
+                return fileRing.openForWriting((fileRing1, data, offset, length) -> true);
+            }
+        }) {
+        };
+
+        os.write("one\n".getBytes("UTF-8"));
+        os.flush();
+        os.write("two\n".getBytes("UTF-8"));
+        os.flush();
+        os.write("three\n".getBytes("UTF-8"));
+        os.flush();
+        os.write("four\n".getBytes("UTF-8"));
+        os.flush();
+
+        try (final InputStream is = fileRing.openForReading()) {
+            try (final InputStreamReader isr = new InputStreamReader(is)) {
+                try (final BufferedReader bis = new BufferedReader(isr)) {
+                    assertEquals("three", bis.readLine());
+                    assertEquals("four", bis.readLine());
+                    assertNull(bis.readLine());
+                }
+            }
+
+        }
+
+        fileRing.delete();
     }
 }
