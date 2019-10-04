5464e8811963b4a4b4ecd856771d54d108563637
Sam Baskinger <sam.baskinger@gmail.com>
2019-10-04 08:04:42 -0500

    ListObject: Clean up the code a little.

--- sdsai-common/src/main/java/com/github/basking2/sdsai/netobj/ListObject.java
+++ sdsai-common/src/main/java/com/github/basking2/sdsai/netobj/ListObject.java
@@ -13,279 +13,270 @@ import com.github.basking2.sdsai.ListVisitor;
 import com.github.basking2.sdsai.RedBlackTree;
 
 /**
- * A simple list. While this implementation sorts the list the reciever
+ * A simple list. While this implementation sorts the list the receiver
  * should never assume any particular order to the list.
  */
-public class ListObject extends NetObject implements Iterable<NetObject>
-{
+public class ListObject extends NetObject implements Iterable<NetObject> {
 
- /**
-  * A list of things that are NetObjects.
-  */
-  protected RedBlackTree<NetObject> list;
+    /**
+     * A list of things that are NetObjects.
+     */
+    protected RedBlackTree<NetObject> list;
 
-  public ListObject(String s, int i, RedBlackTree<NetObject> l)
-  {
-    super(s, i);
-    list = l;
-  }
-
-  /**
-   * Make a list object using the data of list l and type i.
-   */
-  public ListObject(String s, int i, ListObject l)
-  {
-    super(s, i);
-    list = l.list;
-  }
-
-  public ListObject(String s, ListObject l)
-  {
-    super(s, l.type);
-    list = l.list;
-  }
-
- /**
-  * Create a ListObject whose internal tree datastructure is
-  * set to l.
-  * @param l the object that will be set as this ListObject's internal
-  * datastructure.
-  */
-  public ListObject(String s, RedBlackTree<NetObject> l)
-  {
-    super(s, LIST);
-    list = l;
-  }
-
-  public ListObject(String s, int i)
-  {
-    super(s,i);
-    list = new RedBlackTree<NetObject>();
-  }
-
-  public ListObject(String s)
-  {
-    super(s, LIST);
-    list = new RedBlackTree<NetObject>();
-  }
-
-  public NetObject first()
-  {
-    NetObject o = null;
-
-    if(list!=null){
-      RedBlackTree<NetObject>.RBNode n = list.min();
-
-      if(n!=null)
-        o = (NetObject) n.getKey().getData();
+    public ListObject(String s, int i, RedBlackTree<NetObject> l) {
+        super(s, i);
+        list = l;
     }
 
-    return o;
-  }
-
-
-  public NetObject last()
-  {
-    NetObject o = null;
-
-    if(list!=null){
-      RedBlackTree<NetObject>.RBNode n = list.max();
-
-      if(n!=null)
-        o = n.getKey().getData();
+    /**
+     * Make a list object using the data of list l and type i.
+     */
+    public ListObject(String s, int i, ListObject l) {
+        super(s, i);
+        list = l.list;
     }
 
-    return o;
-  }
-
-  public void set(ListObject n)
-  {
-    list.del(new Key<NetObject>(n.name, n));
-    list.add(new Key<NetObject>(n.name, n));
-  }
-
-  public void set(DataObject n)
-  {
-    list.del(new Key<NetObject>(n.name, n));
-    list.add(new Key<NetObject>(n.name, n));
-  }
-
-  public void add(ListObject n)
-  {
-    list.add(new Key<NetObject>(n.name, n));
-  }
-
-  public void add(DataObject n)
-  {
-    list.add(new Key<NetObject>(n.name, n));
-  }
-
-  /**
-   * Add the keys in the internal storage of the ListObject <i>lo</i>
-   * to this ListObject.
-   */
-  public void addTo(ListObject lo)
-  {
-    for ( Key<NetObject> k : lo.list ) 
-      list.add(k);
-  }
-
-  /**
-   * Merge the lo ListObject into this object, deleting objects from this
-   * ListObject that are in lo. 
-   * 
-   * If lo and this list object are the same object, then they are
-   * considered already merged.
-   */
-  public void merge(ListObject lo)
-  {
-    // Base-case of merging to ourselves.
-    if ( this == lo )
-      return;
-    
-    for ( Key<NetObject> k : lo.list ) {
-      
-      if ( k.getData().getType() == NetObject.LIST ) {
-        
-        ListObject klist = (ListObject) k.getData();
-        
-        // Find the list object. If it doesn't exist, set it to klist.
-        // merging klist into klist is considered (see first if() above).
-        findListObject(klist.getName(), klist).merge(klist);
-        
-      } else {
-        list.del(k);
-        list.add(k);
-      }
+    public ListObject(String s, ListObject l) {
+        super(s, l.type);
+        list = l.list;
     }
-  }
-  
-  /**
-   * This will take the internal list of the object <i>lo</i>
-   * and replace the internal list of this ListObject with it.
-   * This is <b>dangerous</b> because you can create object loops
-   * but is a wonderful optimization when reading in objects from files.
-   * @param lo
-   */
-  public void shareInternalData(ListObject lo)
-  {
-    this.list = lo.list;
-  }
-  
-  /** 
-   * Delete the B-Tree substructure and start from scratch. 
-   */
-  public void delAll(){ list = new RedBlackTree<NetObject>(); }
 
-  /**
-   * Create the given path and return the last element in the path.
-   */
-  public ListObject setPath(Object[] path)
-  {
-    NetObject  nobj = this;  // Net object.
-    ListObject lobj = this;  // List object./
+    /**
+     * Create a ListObject whose internal tree data structure is set to l.
+     *
+     * @param l the object that will be set as this ListObject's internal data structure.
+     */
+    public ListObject(String s, RedBlackTree<NetObject> l) {
+        super(s, LIST);
+        list = l;
+    }
 
-    for(int i = 0; i < path.length; i++){
+    public ListObject(String s, int i) {
+        super(s, i);
+        list = new RedBlackTree<>();
+    }
 
-      String name = path[i].toString();
+    public ListObject(String s) {
+        super(s, LIST);
+        list = new RedBlackTree<>();
+    }
 
-      try {
-        nobj = lobj.find(name);
-        
-        /* Is it not a list? */
-        if(nobj.getType() != NetObject.LIST){
-          ListObject tmpobj = new ListObject(name);
+    public NetObject first() {
+        if (list != null) {
+            final RedBlackTree<NetObject>.RBNode n = list.min();
 
-          /* Destroy the object and set it to list. */
-          lobj.set(tmpobj);
-
-          lobj = tmpobj;
-
-        } else {
-          lobj = (ListObject) nobj;
+            if (n != null) {
+                @SuppressWarnings("unchecked") final NetObject o = (NetObject) n.getKey().getData();
+                return o;
+            }
         }
 
-      } catch(ObjectNotFoundException e) {
-        
-        ListObject tmpobj = new ListObject(name);
-
-        lobj.add(tmpobj);
-
-        lobj = tmpobj;
-      }
+        return null;
     }
 
-    return lobj;
-  }
 
-  /**
-   * Create the given path if it does not already exist and set
-   * leaf on the last element in the path.
-   * For example the path { a, b, c} and the NetObject d would
-   * create the list a containing the list b containing the list c
-   * containing the object d.
-   */
-  public void setPath(Object[] path, DataObject leaf)
-  {
-    setPath(path).set(leaf);
-  }
+    public NetObject last() {
+        NetObject o = null;
 
-  public void setPath(Object[] path, ListObject leaf)
-  {
-    setPath(path).set(leaf);
-  }
+        if (list != null) {
+            RedBlackTree<NetObject>.RBNode n = list.max();
 
-  /**
-   * If the path exists, return the last element in it.
-   */
-  public NetObject findPath(Object[] path)
-  throws ObjectNotFoundException, MismatchedTypeException
-  {
-    ListObject l = findParent(path);
-    
-    NetObject no = l.find(path[path.length-1].toString());
-    
-    if ( no == null )
-      throw new ObjectNotFoundException("Object was not found in parent container.", path[path.length-1].toString(), l);
-    
-    return no;
-  }
-  
-  /**
-   * @param path the path to the object whose parent we are trying to find.
-   * @return
-   * @throws ObjectNotFoundException
-   * @throws MismatchedTypeException
-   */
-  public ListObject findParent(Object[] path)
-  throws ObjectNotFoundException, MismatchedTypeException
-  {
-    NetObject  nobj = this;  /* Net object. */
-    ListObject lobj = this;  /* List object. */
-    int         len = path.length - 1;
+            if (n != null)
+                o = n.getKey().getData();
+        }
 
-    for(int i = 0; i < len; i++){
-
-      nobj = lobj.find(path[i].toString());
-        
-      // if we don't find it.
-      if ( nobj == null )
-        throw new ObjectNotFoundException(
-            "Could not find object "+path[i].toString(), 
-            path[i].toString(),
-            lobj);
-
-      // if we aren't done looking and this isn't a list object.
-      if ( nobj.getType() != NetObject.LIST && i < len - 1 )
-        throw new MismatchedTypeException(
-            "Expected list object "+path[i].toString(), 
-            path[i].toString(),
-            lobj);
-      
+        return o;
     }
-    
-    return (ListObject) nobj;
-  }
+
+    public void set(ListObject n) {
+        list.del(new Key<NetObject>(n.name, n));
+        list.add(new Key<NetObject>(n.name, n));
+    }
+
+    public void set(DataObject n) {
+        list.del(new Key<NetObject>(n.name, n));
+        list.add(new Key<NetObject>(n.name, n));
+    }
+
+    public void add(ListObject n) {
+        list.add(new Key<NetObject>(n.name, n));
+    }
+
+    public void add(DataObject n) {
+        list.add(new Key<NetObject>(n.name, n));
+    }
+
+    /**
+     * Add the keys in the internal storage of the ListObject <i>lo</i>
+     * to this ListObject.
+     *
+     * @param lo Add all list object to this object.
+     */
+    public void addTo(final ListObject lo) {
+        for (final Key<NetObject> k : lo.list) {
+            list.add(k);
+        }
+    }
+
+    /**
+     * Merge the lo ListObject into this object, deleting objects from this
+     * ListObject that are in lo.
+     * <p>
+     * If lo and this list object are the same object, then they are
+     * considered already merged.
+     *
+     * @param lo The list object to merge.
+     */
+    public void merge(final ListObject lo) {
+        // Base-case of merging to ourselves.
+        if (this == lo) {
+            return;
+        }
+
+        for (final Key<NetObject> k : lo.list) {
+
+            if (k.getData().getType() == NetObject.LIST) {
+
+                final ListObject klist = (ListObject) k.getData();
+
+                // Find the list object. If it doesn't exist, set it to klist.
+                // merging klist into klist is considered (see first if() above).
+                findListObject(klist.getName(), klist).merge(klist);
+
+            } else {
+                list.del(k);
+                list.add(k);
+            }
+        }
+    }
+
+    /**
+     * This will take the internal list of the object <i>lo</i>
+     * and replace the internal list of this ListObject with it.
+     * This is <b>dangerous</b> because you can create object loops
+     * but is a wonderful optimization when reading in objects from files.
+     *
+     * @param lo The list object to use the internal {@link #list} object.
+     */
+    public void shareInternalData(final ListObject lo) {
+        this.list = lo.list;
+    }
+
+    /**
+     * Delete the B-Tree substructure and start from scratch.
+     */
+    public void delAll() {
+        list = new RedBlackTree<>();
+    }
+
+    /**
+     * Create the given path and return the last element in the path.
+     *
+     * @param path The path.
+     * @return The list object at the bottom of the path.
+     */
+    public ListObject setPath(Object[] path) {
+        NetObject nobj = this;  // Net object.
+        ListObject lobj = this;  // List object./
+
+        for (int i = 0; i < path.length; i++) {
+
+            String name = path[i].toString();
+
+            try {
+                nobj = lobj.find(name);
+
+                /* Is it not a list? */
+                if (nobj.getType() != NetObject.LIST) {
+                    ListObject tmpobj = new ListObject(name);
+
+                    /* Destroy the object and set it to list. */
+                    lobj.set(tmpobj);
+
+                    lobj = tmpobj;
+
+                } else {
+                    lobj = (ListObject) nobj;
+                }
+
+            } catch (ObjectNotFoundException e) {
+
+                ListObject tmpobj = new ListObject(name);
+
+                lobj.add(tmpobj);
+
+                lobj = tmpobj;
+            }
+        }
+
+        return lobj;
+    }
+
+    /**
+     * Create the given path if it does not already exist and set
+     * leaf on the last element in the path.
+     * For example the path { a, b, c} and the NetObject d would
+     * create the list a containing the list b containing the list c
+     * containing the object d.
+     */
+    public void setPath(Object[] path, DataObject leaf) {
+        setPath(path).set(leaf);
+    }
+
+    public void setPath(Object[] path, ListObject leaf) {
+        setPath(path).set(leaf);
+    }
+
+    /**
+     * If the path exists, return the last element in it.
+     */
+    public NetObject findPath(Object[] path)
+            throws ObjectNotFoundException, MismatchedTypeException {
+        ListObject l = findParent(path);
+
+        NetObject no = l.find(path[path.length - 1].toString());
+
+        if (no == null)
+            throw new ObjectNotFoundException("Object was not found in parent container.", path[path.length - 1].toString(), l);
+
+        return no;
+    }
+
+    /**
+     * @param path the path to the object whose parent we are trying to find.
+     * @return The list object.
+     * @throws ObjectNotFoundException If the object is not found.
+     * @throws MismatchedTypeException If it is not the expected type.
+     */
+    public ListObject findParent(final Object[] path)
+            throws ObjectNotFoundException, MismatchedTypeException {
+        NetObject nobj = this;  /* Net object. */
+        ListObject lobj = this;  /* List object. */
+        final int len = path.length - 1;
+
+        for (int i = 0; i < len; i++) {
+
+            nobj = lobj.find(path[i].toString());
+
+            // if we don't find it.
+            if (nobj == null)
+                throw new ObjectNotFoundException(
+                        "Could not find object " + path[i].toString(),
+                        path[i].toString(),
+                        lobj);
+
+            // if we aren't done looking and this isn't a list object.
+            if (nobj.getType() != NetObject.LIST && i < len - 1)
+                throw new MismatchedTypeException(
+                        "Expected list object " + path[i].toString(),
+                        path[i].toString(),
+                        lobj);
+
+        }
+
+        return (ListObject) nobj;
+    }
+
 
   public NetObject find(String n)
   throws ObjectNotFoundException
@@ -319,10 +310,11 @@ public class ListObject extends NetObject implements Iterable<NetObject>
 
   public int size(){ return list.size(); }
 
- /**
-  * Returns a list of Key objects which contain the user data.
-  * To access the user data call getData() on the key object.
-  */
+    /**
+     * Returns a list of Key objects which contain the user data.
+     * To access the user data call getData() on the key object.
+     * @return A list of all network objects.
+     */
   public List<NetObject> toList(){
     List<NetObject> nol = new ArrayList<NetObject>(list.size());
 
@@ -369,10 +361,15 @@ public class ListObject extends NetObject implements Iterable<NetObject>
 
   }
 
- /**
-  * This will find the ListObject named name in this list object.
-  * This will only return defined ListObject, never null.
-  */
+    /**
+     * This will find the ListObject named name in this list object.
+     * This will only return defined ListObject, never null.
+     *
+     * @param name The name.
+     * @return The found list object.
+     * @throws ObjectNotFoundException When the object is not found.
+     * @throws MismatchedTypeException If the type is not a list object.
+     */
   public ListObject findListObject(String name)
     throws ObjectNotFoundException, MismatchedTypeException
   {
@@ -389,13 +386,14 @@ public class ListObject extends NetObject implements Iterable<NetObject>
     return (ListObject) no;
   }
   
-  /**
-   * Return the found object. If the object is not found or is the wrong
-   * type of object, it is replaced with def and def is returned.
-   * @param name
-   * @param def returned if the submitted name cannot be found.
-   * @return
-   */
+    /**
+     * Return the found object. If the object is not found or is the wrong
+     * type of object, it is replaced with def and def is returned.
+     *
+     * @param name The name.
+     * @param def The default.
+     * @return The list of matching names or the default if none are found.
+     */
   public ListObject findListObject(String name, ListObject def)
   {
     try
@@ -665,5 +663,5 @@ public class ListObject extends NetObject implements Iterable<NetObject>
       public void      remove()  { i.remove(); }
     };
   }
-   
 }
+   
